98e78e7e9b27942a8f1fc48208f4fa4dca7805b4
zvika <zhsegal@gmail.com>
2021-11-09 13:32:47 +0200

    1nsf

--- server.js
+++ server.js
@@ -14,8 +14,19 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const patientSchema = new mongoose.Schema({
     PatientID: String,
-    q2: String,
-    q3: String,
+    AGE: Number,
+    GENDER: String,
+    Chornic_conditions:[{}],
+    POEM1: Number,
+    POEM2: Number,
+    POEM3: Number,
+    POEM4: Number,
+    POEM5: Number,
+    POEM6: Number,
+    POEM7: Number,
+   
+    
+
 })
 
 const Patient = mongoose.model("Patient", patientSchema)
@@ -45,9 +56,19 @@ app.post('/dr', (req, res) => {
         }
 
         else{
+            var poem_score=docs[0].POEM1+docs[0].POEM2+docs[0].POEM3+docs[0].POEM4+docs[0].POEM5+docs[0].POEM6+docs[0].POEM7
+            
             console.log(docs.length)
             console.log(docs[0].PatientID)
-            res.render('results',{ID: docs[0].PatientID})
+            res.render('results',
+            {ID: docs[0].PatientID,
+            AGE:docs[0].AGE,
+            GENDER:docs[0].GENDER,
+            CONDITIONS:docs[0].Chornic_conditions,
+            POEM: poem_score
+
+            
+            })
         }
 
 
@@ -58,15 +79,24 @@ app.post('/dr', (req, res) => {
 
 })
 app.post('/', (req, res) => {
-    
-    console.log(req.body)
+  
     var patient=new Patient({
-        PatientID:req.body.question1,
-            q2: req.body.question2,
-            q3: req.body.question3,
+        PatientID:req.body.ID,
+        AGE: req.body.AGE,
+        GENDER: req.body.GENDER,
+        Chornic_conditions: req.body.CHRONIC_CONDITIONS,
+        POEM1: req.body.POEM1,
+        POEM2: req.body.POEM2,
+        POEM3: req.body.POEM3,
+        POEM4: req.body.POEM4,
+        POEM5: req.body.POEM5,
+        POEM6: req.body.POEM6,
+        POEM7: req.body.POEM7,
+       
 
     })
     
+
     patient.save()
     console.log(patient)
     res.send('ok thats amayzing')
