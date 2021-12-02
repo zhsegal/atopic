c15b0b458de9ef9cda7aec0773806bdcbfd3c409
zvika <zhsegal@gmail.com>
2021-12-02 12:08:20 +0200

    'fdsaaff'

--- server.js
+++ server.js
@@ -43,19 +43,61 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 
+
 app.get('/', (req, res) => {
-    
-    res.sendFile('public/index.html', { root: __dirname })
+    var poem = [
+        { id: 'מה קורה אחי', a1: "טוב", a2:'רע',name:'POEM1' },
+        { id: 'מה קורה בלך', a1: "חי", a2:'גע',name:'POEM2' },
+        { id: 'מה קורה גבר', a1: "יייי", a2:'גגג',name:'POEM3' },
+        { id: 'מה קורה גבר', a1: "יייי", a2:'גגג',name:'POEM4' },
+        { id: 'מה קורה גבר', a1: "יייי", a2:'גגג',name:'POEM5' },
+        { id: 'מה קורה גבר', a1: "יייי", a2:'גגג',name:'POEM6' },
+        { id: 'מה קורה גבר', a1: "יייי", a2:'גגג',name:'POEM7' },
+    ];
+
+    var adct = [
+        { id: 'מה קורה ggי', a1: "טוב", a2:'רע',name:'ACDT1' },
+        { id: 'ggg קורה בלך', a1: "חי", a2:'גע',name:'ACDT2' },
+        { id: 'מהggg קורה גבר', a1: "יייי", a2:'גגג',name:'ACDT3' },
+        { id: 'מה קggורה גבר', a1: "יייי", a2:'גגג',name:'ACDT4' },
+        { id: 'מה קggורה גבר', a1: "יייי", a2:'גגג',name:'ACDT5' },       
+    ];
+    res.render('home',{poem,adct})
 });
 
-app.get('/try', (req, res) => {
-    var data = [
-        { id: 'מה קורה אחי', a1: "טוב", a2:'רע',name:'P1' },
-        { id: 'מה קורה בלך', a1: "חי", a2:'גע',name:'P2' },
-        { id: 'מה קורה גבר', a1: "יייי", a2:'גגג',name:'P3' },
-    ];
-    res.render('home',{data})
-});
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    var patient=new Patient({
+        PatientID:req.body.ID,
+        AGE: req.body.AGE,
+        GENDER: req.body.GENDER,
+        Chornic_conditions: req.body.CHRONIC_CONDITIONS,
+        POEM1: req.body.POEM1,
+        POEM2: req.body.POEM2,
+        POEM3: req.body.POEM3,
+        POEM4: req.body.POEM4,
+        POEM5: req.body.POEM5,
+        POEM6: req.body.POEM6,
+        POEM7: req.body.POEM7,
+        ACDT1: req.body.ACDT1,
+        ACDT2: req.body.ACDT2,
+        ACDT3: req.body.ACDT3,
+        ACDT4: req.body.ACDT4,
+        ACDT5: req.body.ACDT5,
+        ACDT6: req.body.ACDT6,
+        PRURITUS:req.body.pruritus
+       
+
+    })
+    
+
+    patient.save()
+    console.log('patient:')
+    console.log(patient)
+    res.send(req.body)
+
+})
 
 
 app.get('/dr', (req, res) => {
@@ -98,45 +140,8 @@ app.post('/dr', (req, res) => {
    
 
 })
-app.post('/', (req, res) => {
-    console.log(req.body)
-    var patient=new Patient({
-        PatientID:req.body.ID,
-        AGE: req.body.AGE,
-        GENDER: req.body.GENDER,
-        Chornic_conditions: req.body.CHRONIC_CONDITIONS,
-        POEM1: req.body.POEM1,
-        POEM2: req.body.POEM2,
-        POEM3: req.body.POEM3,
-        POEM4: req.body.POEM4,
-        POEM5: req.body.POEM5,
-        POEM6: req.body.POEM6,
-        POEM7: req.body.POEM7,
-        ACDT1: req.body.ACDT1,
-        ACDT2: req.body.ACDT2,
-        ACDT3: req.body.ACDT3,
-        ACDT4: req.body.ACDT4,
-        ACDT5: req.body.ACDT5,
-        ACDT6: req.body.ACDT6,
-        PRURITUS:req.body.pruritus
-       
 
-    })
-    
 
-    patient.save()
-    console.log(patient)
-    res.send('תודה על מילוי השאלון!')
 
-})
-
-app.post('/try', (req, res) => {
-    console.log(req.body)
-   
-    
-
-    res.send(req.body)
-
-})
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
